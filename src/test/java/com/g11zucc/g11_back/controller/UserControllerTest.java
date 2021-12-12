272562c9ab7c6809ae6ec56a903f6eb64785b1f5
wlx <31901209@stu.zucc.edu.cn>
2021-12-12 14:14:47 +0800

    测试样例

--- src/test/java/com/g11zucc/g11_back/controller/UserControllerTest.java
+++ src/test/java/com/g11zucc/g11_back/controller/UserControllerTest.java
@@ -1,18 +1,25 @@
 package com.g11zucc.g11_back.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.g11zucc.g11_back.common.api.ApiResult;
 import com.g11zucc.g11_back.model.entity.user;
+import com.g11zucc.g11_back.service.IuserService;
+import com.g11zucc.g11_back.utils.MD5Utils;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import java.net.URI;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,8 +29,13 @@ class UserControllerTest {
     @Autowired
     private WebApplicationContext wac;
 
+    @Autowired
+    private IuserService userService;
+
+
     @BeforeEach
     void setUp() {
+
     }
 
     @Test
@@ -60,14 +72,47 @@ class UserControllerTest {
 
     @Test
     void save() {
+        userService.getBaseMapper().deleteById("testg12");
+        Date date = new Date();
+        user u=new user();
+        u.setUserId("testg12");
+        u.setUserName("Test");
+        u.setUserPwd("123456");
+        u.setUserRegistertime(date);
+        u.setUserState("ZC");
+        u.setUserSex("WZ");
+        u.setUserJurisdiction("GLY");
+        u.setUserSignature("CSYL");
+        userService.getBaseMapper().insert(u);
+        List<user> result=userService.list(new LambdaQueryWrapper<user>().eq(user::getUserId,"testg12"));
+        if(!result.isEmpty())
+            System.out.println("save成功读入数据！");
     }
 
     @Test
     void update() {
+        user u = new user();
+        u.setUserId("31901209");
+        u.setUserSignature("update编译通过！");
+        userService.getBaseMapper().updateById(u);
+        user user=userService.getBaseMapper().selectById("31901209");
+        System.out.println(user.getUserSignature());
+    }
+
+
+    @Test
+    void reset() {
+        user u = new user();
+        u.setUserId("testg12");
+        u.setUserPwd(MD5Utils.getPwd("123456"));
+        userService.getBaseMapper().updateById(u);
+        List<user> result=userService.list(new LambdaQueryWrapper<user>().eq(user::getUserId,"testg12"));
+        System.out.println(result.get(0).getUserPwd());
     }
 
     @Test
     void deleteuser() {
+        userService.getBaseMapper().deleteById("testg12");
     }
 
     @Test
@@ -107,7 +152,4 @@ class UserControllerTest {
        System.out.println("findPage测试成功！");
     }
 
-    @Test
-    void reset() {
-    }
 }
