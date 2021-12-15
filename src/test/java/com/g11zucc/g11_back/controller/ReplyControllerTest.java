ce9c9e1592d6bd70d8ca42de969fff219ca4c066
ZUCC-zhj <1325060469@qq.com>
2021-12-15 12:04:50 +0800

    111

--- src/test/java/com/g11zucc/g11_back/controller/ReplyControllerTest.java
+++ src/test/java/com/g11zucc/g11_back/controller/ReplyControllerTest.java
@@ -1,21 +1,9 @@
 package com.g11zucc.g11_back.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-<<<<<<< HEAD
+
 import com.g11zucc.g11_back.model.entity.reply;
-import com.g11zucc.g11_back.model.entity.wall;
 import com.g11zucc.g11_back.service.IreplyService;
-=======
-import com.g11zucc.g11_back.common.RestResponse;
-import com.g11zucc.g11_back.model.entity.email;
-import com.g11zucc.g11_back.model.entity.reply;
-import com.g11zucc.g11_back.model.entity.user;
-import com.g11zucc.g11_back.model.entity.wall;
-import com.g11zucc.g11_back.service.IreplyService;
-import com.g11zucc.g11_back.service.IwallService;
-import com.g11zucc.g11_back.service.MailService;
-import com.g11zucc.g11_back.utils.MD5Utils;
->>>>>>> origin/master
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +16,6 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.Date;
 import java.util.List;
 
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
@@ -101,16 +86,16 @@ class ReplyControllerTest {
         r.setReplyUsername("110");
         r.setReplyState("00");
         r.setWallId(1);
-        replyService.getBaseMapper().insert(r);
+        replyservice.getBaseMapper().insert(r);
         LambdaQueryWrapper<reply> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(reply::getId, 108);
-        List<reply> result = replyService.list(queryWrapper);
+        List<reply> result = replyservice.list(queryWrapper);
         if(!result.isEmpty()){
             System.out.println("添加评论信息成功！");
-            replyService.getBaseMapper().deleteById(r);
+            replyservice.getBaseMapper().deleteById(r);
             LambdaQueryWrapper<reply> queryWrapper2 = new LambdaQueryWrapper<>();
             queryWrapper2.eq(reply::getId, 108);
-            List<reply> result2 = replyService.list(queryWrapper2);
+            List<reply> result2 = replyservice.list(queryWrapper2);
             if(!result2.isEmpty())
                 System.out.println("删除评论信息失败！");
             else System.out.println("删除评论信息成功！");}
