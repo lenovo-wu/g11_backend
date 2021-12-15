a7efdbf0a2cfe293d6ac697923a4be82e3cc6a4d
wlx <31901209@stu.zucc.edu.cn>
2021-12-15 10:30:45 +0800

    收藏后端修改

--- src/main/java/com/g11zucc/g11_back/controller/CollectionController.java
+++ src/main/java/com/g11zucc/g11_back/controller/CollectionController.java
@@ -1,6 +1,7 @@
 package com.g11zucc.g11_back.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.g11zucc.g11_back.common.api.ApiResult;
@@ -25,8 +26,18 @@ public class CollectionController extends  BaseController{
     @PostMapping("/insertCollection")
     public ApiResult<?> save(@RequestBody collection c){
         c.setCollectionTime(new Date(System.currentTimeMillis()));
-        collectionService.getBaseMapper().insert(c);
-        return ApiResult.success();
+        LambdaQueryWrapper<collection> lanwrapper = new LambdaQueryWrapper<>();
+        lanwrapper.eq(collection::getCollectionUserid,c.getCollectionUserid())
+                .eq(collection::getCollectionWallid,c.getCollectionWallid());
+        List<collection> list=null;
+        list= collectionService.list(lanwrapper);
+        if(list.size()==0){
+            collectionService.getBaseMapper().insert(c);
+            return ApiResult.success();
+        }
+        else{
+            return ApiResult.failed("重复收藏！");
+        }
     }
 
     @DeleteMapping("/delete/{collId}")
